e52d245cdfb8a8700c16474b93decf8b79af48c9
Max Troughear <max@maxtroughear.dev>
2020-12-29 16:24:39 +1300

    update packages and enable cors for development

--- middleware.go
+++ middleware.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/kiwisheets/auth"
 	"github.com/kiwisheets/util"
@@ -9,4 +10,14 @@ import (
 
 func registerMiddleware(router *gin.RouterGroup, db *gorm.DB, cfg *util.GqlConfig) {
 	router.Use(auth.Middleware())
+
+	// register cors middleware for Apollo Studio if in Dev
+	if cfg.Environment == "development" {
+		config := cors.DefaultConfig()
+		config.AllowOrigins = []string{
+			"https://studio.apollographql.com",
+		}
+		config.AllowCredentials = true
+		router.Use(cors.New(config))
+	}
 }
